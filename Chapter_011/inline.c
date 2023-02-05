be42fa6d3ac11845584e1df974ab8cfaff572b7b
enginearn <enginyaaaaan@gmail.com>
2023-02-06 07:20:46 +0900

    Added

---
+++ Chapter_011/inline.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+inline int add(int a, int b)
+{
+    return a + b;
+}
+
+int main(void)
+{
+    printf("a + b = %d\n", add(1, 2));
+    printf("a + b = %d\n", add(add(3, 4), 5));
+    printf("a + b = %d\n", add(add(6, 7), add(8, 9)));
+}
