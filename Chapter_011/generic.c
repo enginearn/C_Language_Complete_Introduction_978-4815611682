387cc200d7e8139c7bd558965b69cab0b30c6a7d
enginearn <enginyaaaaan@gmail.com>
2023-02-12 12:59:41 +0900

    Added

--- Chapter_011/generic.c
+++ Chapter_011/generic.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+int minus_i(int x)
+{
+    printf("int: ");
+    return -x;
+}
+
+double minus_d(double x)
+{
+    printf("double: ");
+    return -x;
+}
+
+#define minus(x) _Generic((x), \
+    int: minus_i, \
+    default: minus_d \
+)((x))
+
 int main(void)
 {
-    
+    printf("%d\n", minus_i(123));
+    printf("%g\n", minus_d(1.23));
+    printf("%d\n", minus(123));
+    printf("%g\n", minus(1.23));
+    printf("%g\n", minus(1.23f));
 }
