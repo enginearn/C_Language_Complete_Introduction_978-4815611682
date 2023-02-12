040e0828a996ca55252b9f0ad9f706c15154bf3d
enginearn <enginyaaaaan@gmail.com>
2023-02-13 08:34:26 +0900

    Added

--- Chapter_011/generic.c
+++ Chapter_011/generic.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int add_i(int x, int y)
+{
+    printf("int: ");
+    return x + y;
+}
+
+double add_d(double x, double y)
+{
+    printf("double: ");
+    return x + y;
+}
+
 int minus_i(int x)
 {
     printf("int: ");
@@ -17,6 +29,11 @@ double minus_d(double x)
     default: minus_d \
 )((x))
 
+#define add(x, y) _Generic((x), \
+    int: add_i, \
+    default: add_d \
+)((x), (y))
+
 int main(void)
 {
     printf("%d\n", minus_i(123));
@@ -24,4 +41,10 @@ int main(void)
     printf("%d\n", minus(123));
     printf("%g\n", minus(1.23));
     printf("%g\n", minus(1.23f));
+    printf("--------------------\n");
+    printf("%d\n", add_i(123, 456));
+    printf("%g\n", add_d(1.23, 4.56));
+    printf("%d\n", add(123, 456));
+    printf("%g\n", add(1.23, 4.56));
+    printf("%g\n", add(1.23f, 4.56f));
 }
