3136a6975cf09216c2dd7a6ed6826a495912b5cd
enginearn <enginyaaaaan@gmail.com>
2023-03-07 07:00:12 +0900

    Added

---
+++ Chapter_012/padding.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef struct {
+    char c;
+    short s;
+    int i;
+} A;
+
+typedef struct {
+    char c;
+    int i;
+    short s;
+} B;
+
+int main(void)
+{
+    A a;
+    printf("%lu\n", sizeof(a));
+    printf("%p\n", &a);
+    printf("%p\n", &a.c);
+    printf("%p\n", &a.s);
+    printf("%p\n", &a.i);
+
+    puts("");
+
+    B b;
+    printf("%lu\n", sizeof(b));
+    printf("%p\n", &b);
+    printf("%p\n", &b.c);
+    printf("%p\n", &b.i);
+    printf("%p\n", &b.s);
+}
